3e06db4e39977fddee53fd6ad7b225df7d4365ff
abudhosamuel <abudhosamuel@gmail.com>
2024-07-03 17:23:09 +0300

    Index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the paragraph element with id="text"
+    const textElement = document.getElementById("text");
+    
+    // Change the text content of the paragraph element
+    textElement.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  
